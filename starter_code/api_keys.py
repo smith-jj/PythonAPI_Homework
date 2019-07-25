5e8dd841797306610bdee23d8667219019c8dfc0
smith-jj <50722440+smith-jj@users.noreply.github.com>
2019-07-25 16:43:43 -0500

    Oops
    Sorry i lost my mind on this one

--- starter_code/api_keys.py
+++
@@ -1,2 +0,0 @@
-# OpenWeatherMap API Key - WeatherPy
-api_key = "af3be7201cd7cfbed553a4f443e3d489"
---
+++ starter_code/weather.py
@@ -0,0 +1,2 @@
+# OpenWeatherMap API Key - WeatherPy
+api_key = "28a7784eba5a0007d41501f0010ec4fa"
